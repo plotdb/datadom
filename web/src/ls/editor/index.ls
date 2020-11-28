ece297d59056aa2b179cc5d682dd02110f1d92f7
Kirby Wu <tkirby@gmail.com>
2020-11-28 16:19:24 +0800

    update demo with sharedb

--- web/src/ls/editor/index.ls
+++ web/src/ls/editor/index.ls
@@ -1,29 +1,64 @@
 (->
-  getfa 'sample'
-    .then (fs) ->
-      fs.write-file-sync(
-        \blank,
-        """
-        //- pug
-        doctype html
-        html
-          head
-          body
-            h1 hello world!
-        """
-      )
-      ed = new Editor do
-        node: do
-          edit: '[ld=editor]'
-          view: '[ld=viewer]'
-        editlet: {}
-        renderer: ({fs}) ->
-          if !fs => return
-          payload = html: (fs.read-file-sync 'blank' .toString!)
-          for k,v of payload =>
-            ret = transpiler.detect(v)
-            if ret.mod and ret.mod.transform => payload[k] = ret.mod.transform v
-          return payload
-      ed.set-files fs
-      ed.open \blank
+  lc = {}
+  sdb = new sharedb-wrapper!
+  watch = (ops, source) ->
+    json0.type.apply lc.data, ops
+    remote = ld$.find '[ld=remote]', 0
+    remote.innerHTML = ""
+    datadom.deserialize lc.data
+      .then ({node, promise}) ->
+        remote.appendChild node
+        console.log ops, source
+
+  sdb.get {id: \sample, watch: watch, create: -> {} }
+    .then (doc) ->
+      lc.doc = doc
+      lc.data = JSON.parse(JSON.stringify(doc.data))
+
+      getfa 'sample'
+        .then (fs) ->
+          fs.write-file-sync(
+            \blank,
+            """
+            //- pug
+            doctype html
+            html
+              head
+              body
+                h1 hello world!
+            """
+          )
+          render-datadom = (code) ->
+            div = document.createElement("div")
+            div.innerHTML = code
+            json = datadom.serialize(div)
+            node = ld$.find '[ld=json]', 0
+            node.innerText = JSON.stringify(json, null, 2)
+
+            domroot = ld$.find '[ld=dom]', 0
+            datadom.deserialize(json)
+              .then ({node, promise}) ->
+                domroot.innerHTML = ""
+                domroot.appendChild node
+            console.log json
+            ops = json0.diff doc.data, json
+            doc.submitOp ops
+
+
+          ed = new Editor do
+            node: do
+              edit: '[ld=editor]'
+              view: '[ld=viewer]'
+            editlet: {}
+            renderer: ({fs}) ->
+              if !fs => return
+              payload = html: (fs.read-file-sync 'blank' .toString!)
+              for k,v of payload =>
+                ret = transpiler.detect(v)
+                if ret.mod and ret.mod.transform => payload[k] = ret.mod.transform v
+                render-datadom(payload.html)
+              return payload
+          ed.set-files fs
+          ed.open \blank
+
 )!
--- web/static/js/editor/index.js
+++ web/static/js/editor/index.js
@@ -1,33 +1,77 @@
 (function(){
-  return getfa('sample').then(function(fs){
-    var ed;
-    fs.writeFileSync('blank', "//- pug\ndoctype html\nhtml\n  head\n  body\n    h1 hello world!");
-    ed = new Editor({
-      node: {
-        edit: '[ld=editor]',
-        view: '[ld=viewer]'
-      },
-      editlet: {},
-      renderer: function(arg$){
-        var fs, payload, k, v, ret;
-        fs = arg$.fs;
-        if (!fs) {
-          return;
-        }
-        payload = {
-          html: fs.readFileSync('blank').toString()
-        };
-        for (k in payload) {
-          v = payload[k];
-          ret = transpiler.detect(v);
-          if (ret.mod && ret.mod.transform) {
-            payload[k] = ret.mod.transform(v);
-          }
-        }
-        return payload;
-      }
+  var lc, sdb, watch;
+  lc = {};
+  sdb = new sharedbWrapper();
+  watch = function(ops, source){
+    var remote;
+    json0.type.apply(lc.data, ops);
+    remote = ld$.find('[ld=remote]', 0);
+    remote.innerHTML = "";
+    return datadom.deserialize(lc.data).then(function(arg$){
+      var node, promise;
+      node = arg$.node, promise = arg$.promise;
+      remote.appendChild(node);
+      return console.log(ops, source);
     });
-    ed.setFiles(fs);
-    return ed.open('blank');
+  };
+  return sdb.get({
+    id: 'sample',
+    watch: watch,
+    create: function(){
+      return {};
+    }
+  }).then(function(doc){
+    lc.doc = doc;
+    lc.data = JSON.parse(JSON.stringify(doc.data));
+    return getfa('sample').then(function(fs){
+      var renderDatadom, ed;
+      fs.writeFileSync('blank', "//- pug\ndoctype html\nhtml\n  head\n  body\n    h1 hello world!");
+      renderDatadom = function(code){
+        var div, json, node, domroot, ops;
+        div = document.createElement("div");
+        div.innerHTML = code;
+        json = datadom.serialize(div);
+        node = ld$.find('[ld=json]', 0);
+        node.innerText = JSON.stringify(json, null, 2);
+        domroot = ld$.find('[ld=dom]', 0);
+        datadom.deserialize(json).then(function(arg$){
+          var node, promise;
+          node = arg$.node, promise = arg$.promise;
+          domroot.innerHTML = "";
+          return domroot.appendChild(node);
+        });
+        console.log(json);
+        ops = json0.diff(doc.data, json);
+        return doc.submitOp(ops);
+      };
+      ed = new Editor({
+        node: {
+          edit: '[ld=editor]',
+          view: '[ld=viewer]'
+        },
+        editlet: {},
+        renderer: function(arg$){
+          var fs, payload, k, v, ret;
+          fs = arg$.fs;
+          if (!fs) {
+            return;
+          }
+          payload = {
+            html: fs.readFileSync('blank').toString()
+          };
+          for (k in payload) {
+            v = payload[k];
+            ret = transpiler.detect(v);
+            if (ret.mod && ret.mod.transform) {
+              payload[k] = ret.mod.transform(v);
+            }
+            renderDatadom(payload.html);
+          }
+          return payload;
+        }
+      });
+      ed.setFiles(fs);
+      return ed.open('blank');
+    });
   });
 })();
